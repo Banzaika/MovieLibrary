9d492cafb6022c5cdaf656d2ed011df85e78636e
Banzaika <117360222+Banzaika@users.noreply.github.com>
2022-12-13 12:51:03 +0300

    Delete env.sh

--- my_movie/my_movie/env.sh
+++
@@ -1,6 +0,0 @@
-export SECRET_KEY=django-insecure-k9ypdn!zj&55c_8@ik&=fhf)%t@&dn@kx6f=978m-kgzdp+^y&
-export PS_DB_NAME=movie
-export PS_USER_NAME=postgres
-export PS_DB_PASS=mypost
-export PS_HOST=127.0.0.1
-export PS_PORT=5432
